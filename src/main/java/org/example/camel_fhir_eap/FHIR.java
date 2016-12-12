5c4a99cf0e757f0ccd868dd859ea1af60b896e05
sigreen <sigreen@redhat.com>
2016-12-12 16:24:49 -0500

    Updated transformation

--- src/main/java/org/example/camel_fhir_eap/FHIR.java
+++ src/main/java/org/example/camel_fhir_eap/FHIR.java
@@ -1,21 +1,79 @@
 package org.example.camel_fhir_eap;
 
 public class FHIR {
+
+	private String patientId;
+	private String componentCode;
+	private String componentDisplay;
+	private String valueQtyValue;
+	private String valueQtyUnit;
 	
-	private String status;
+	public String getPatientId() {
+		return patientId;
+	}
+
+
+
+	public void setPatientId(String patientId) {
+		this.patientId = patientId;
+	}
+
+
+
+	public String getComponentCode() {
+		return componentCode;
+	}
+
+
+
+	public void setComponentCode(String componentCode) {
+		this.componentCode = componentCode;
+	}
+
+
+
+	public String getComponentDisplay() {
+		return componentDisplay;
+	}
+
+
+
+	public void setComponentDisplay(String componentDisplay) {
+		this.componentDisplay = componentDisplay;
+	}
+
+
+
+	public String getValueQtyValue() {
+		return valueQtyValue;
+	}
+
+
+
+	public void setValueQtyValue(String valueQtyValue) {
+		this.valueQtyValue = valueQtyValue;
+	}
+
+
+
+	public String getValueQtyUnit() {
+		return valueQtyUnit;
+	}
+
+
+
+	public void setValueQtyUnit(String valueQtyUnit) {
+		this.valueQtyUnit = valueQtyUnit;
+	}
+
 
 	@Override
 	public String toString() {
-		return "FHIR [status=" + status + "]";
+		return "FHIR [patientId=" + patientId + ", componentCode=" + componentCode + ", componentDisplay="
+				+ componentDisplay + ", valueQtyValue=" + valueQtyValue + ", valueQtyUnit=" + valueQtyUnit + "]";
 	}
 
-	public String getStatus() {
-		return status;
-	}
 
-	public void setStatus(String status) {
-		this.status = status;
-	}
 
 	public FHIR() {
 		// TODO Auto-generated constructor stub
